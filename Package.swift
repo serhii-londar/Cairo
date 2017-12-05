5962e677b158797b532f71e0dfaf701133a75615
Serhii Londar <serhii.londar@gmail.com>
2017-12-05 23:41:25 +0200

    Changed dependencies

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "Cairo",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Cairo",
             targets: ["Cairo"]),
@@ -14,11 +13,9 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/PureSwift/CCairo.git", .branch("master")),
         .package(url: "https://github.com/PureSwift/CFontConfig.git", .branch("master")),
-        .package(url: "https://github.com/serhii-londar/CFreeType.git", .branch("master"))
+        .package(url: "https://github.com/PureSwift/CFreeType.git", .branch("master"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Cairo",
             dependencies: []),
